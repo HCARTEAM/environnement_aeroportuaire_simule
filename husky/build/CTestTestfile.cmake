54dc160a23ebeaf66fa675a01b82d3e95ae79aea
mvo <michaelvo@gmx.com>
2018-04-27 18:18:01 +0200

    Husky v1

---
+++ husky/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/phoenix/ProjetUE/husky/src
+# Build directory: /home/phoenix/ProjetUE/husky/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
+subdirs(husky_control)
+subdirs(husky_description)
+subdirs(husky_gazebo)
+subdirs(husky_viz)
---
+++ husky/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/phoenix/ProjetUE/husky/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/phoenix/ProjetUE/husky/devel;/opt/ros/kinetic".split(';') if "/home/phoenix/ProjetUE/husky/devel;/opt/ros/kinetic" != "" else []
