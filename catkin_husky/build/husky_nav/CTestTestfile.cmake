7d5c3d02cd5e7f2c0897af970fc576753ec92ca0
mvo <michaelvo@gmx.com>
2018-05-21 17:04:38 +0200

    v1

--- catkin_husky/build/husky_nav/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/phoenix/ProjetUE/AirportEnvironment/catkin_husky/src/husky_nav
-# Build directory: /home/phoenix/ProjetUE/AirportEnvironment/catkin_husky/build/husky_nav
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
